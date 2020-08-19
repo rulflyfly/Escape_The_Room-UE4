d5e24a09c4b5b62e00223306d05e6e99f440d6a8
rulflyfly <rulflyfly@gmail.com>
2020-08-19 22:01:36 +0300

    added a trigger volume

--- Source/Escape_the_room/OpenDoor.h
+++ Source/Escape_the_room/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -24,5 +25,9 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 60.f;
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
 };
