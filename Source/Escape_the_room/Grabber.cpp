c009d673f40f7f2de2a68e68fe09d9bc2bf94de5
rulflyfly <rulflyfly@gmail.com>
2020-08-24 01:09:06 +0300

    work on collitions and lifting objects in progress

--- Source/Escape_the_room/Grabber.cpp
+++ Source/Escape_the_room/Grabber.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Grabber.h"
+#include "DrawDebugHelpers.h"
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -30,6 +32,43 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player viewpoint this tick
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+         OUT PlayerViewPointLocation,
+         OUT PlayerViewPointRotation
+);
+    
+    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+    
+    // Draw a red line to visualise
+    
+    DrawDebugLine(
+            GetWorld(),
+            PlayerViewPointLocation,
+            LineTraceEnd,
+            FColor(255, 0, 0),
+            false,
+            0.f,
+            0.f,
+            10.f
+            );
+    //  Setup query parameters
+    
+    FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+    // Line-trace (aka ray-cast) out to reach distance
+    
+    FHitResult Hit;
+    GetWorld()->LineTraceSingleByObjectType(
+                    OUT Hit,
+                    PlayerViewPointLocation,
+                    LineTraceEnd,
+                    FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+                    TraceParameters
+                    );
+    
+    
+    // See what we hit
 }
 
